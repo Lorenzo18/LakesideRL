32e5776217afb58c7234f040617fe9a815286202
Dennis <36346762+Lorenzo20@users.noreply.github.com>
2018-02-11 02:08:12 +0100

    Add files via upload

---
+++ lakeside-copyright.lua
@@ -0,0 +1,8 @@
+------------------------------
+------- (c) 2017-2019 --------
+--------- by Lorenzo ---------
+-- and Lakeside MTA:RL Crew --
+------------------------------
+
+
+--  Der Gesamte Lakeside Reallife Gamemode sowie Bild Datein sind rechtlich geschützt und dürfen weder geklaut noch verwendet werden.

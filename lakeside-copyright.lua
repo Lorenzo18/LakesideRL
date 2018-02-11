9bff9e0582e1bc25b6e13273dcabf129f3105d44
Dennis <36346762+Lorenzo20@users.noreply.github.com>
2018-02-11 02:18:06 +0100

    Delete lakeside-copyright.lua

--- lakeside-copyright.lua
+++
@@ -1,8 +0,0 @@
-------------------------------
-------- (c) 2017-2019 --------
---------- by Lorenzo ---------
--- and Lakeside MTA:RL Crew --
-------------------------------
-
-
---  Der Gesamte Lakeside Reallife Gamemode sowie Bild Datein sind rechtlich geschützt und dürfen weder geklaut noch verwendet werden.
